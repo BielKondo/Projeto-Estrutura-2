2907238c6e1fc633970da3a53ad9fda3ca74e285
Gabriel <gabriel.hideaquy@gmail.com>
2025-05-02 15:10:32 -0300

    Métodos Node.java

---
+++ Apl2_arquivosNecessarios/src/apl2/Node.java
@@ -0,0 +1,74 @@
+// arquivo: src/apl2/Node.java
+
+// Eduardo Kenji Hernandes Ikematu - 10439924; Gabriel Hideaquy Kondo - 10436238; João Pedro Sinzato Bocchini - 10440034
+
+package apl2;
+
+// -- A classe Node (que pertence ao pacote apl2) deve conter os atributos que
+// representam a nova versão dos dados de uma pessoa, conforme descrito no
+// enunciado da atividade Apl2.
+// -- A classe deve conter os construtores apropriados, assim como os métodos
+// getters e setters.
+// -- A classe também representa um nó que é usado na implementação da lista
+// duplamente encadeada (classe DLinkedList).
+// -- A classe deve sobrescrever (override) o método public String toString()
+// {...}, retornando uma string com os valores dos atributos da classe.
+
+public class Node {
+	
+	private String ID;
+	private String nome;
+	private float nota;
+	private Node next;
+
+	public Node() {
+		this.ID = null;
+		this.nome = null;
+		this.nota = 0.0f;
+		this.next = null;
+	}
+
+	public Node(String ID, String nome, float nota, Node next) {
+		this.ID = ID;
+		this.nome = nome;
+		this.nota = nota;
+		this.next = next;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getNota() {
+		return nota;
+	}
+
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + ID + ", Nome: " + nome + ", Nota: " + nota;
+	}
+}
